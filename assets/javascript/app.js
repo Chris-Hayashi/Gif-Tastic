a5b9c992ba2729a1446f5d2b77fe74cca545d02b
Christopher Hayashi <Chrisrhayashi@gmail.com>
2019-10-29 15:22:23 -0700

    added input and submit buttons to index.html

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+//Create a variable to store the API key
+
+//Create a variable to store the query URL
+
+//Create a variable called topics to store an array of strings
+
+//Create a for loop to iterate through topics
+
+    //Create a button and store in a variable
+
+    //Give the button a val() equal to the string
+
+    //Give the button a text content equal to the string
+
+//Create an onClick event for the buttons
